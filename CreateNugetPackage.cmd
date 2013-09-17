04a7e149e1be61cd473b10e96370e35c217a8693
jasonall <jasonall@outlook.com>
2013-09-17 14:59:14 -0700

    Added support for Windows Phone 7 applications

--- CreateNugetPackage.cmd
+++ CreateNugetPackage.cmd
@@ -1,1 +1,2 @@
-nuget pack Funcursive\Funcursive.csproj -Prop Configuration=Release
+tools\nuget pack Funcursive\Funcursive.csproj -Prop Configuration=Release
+tools\nuget pack Funcursive7\Funcursive7.csproj -Prop Configuration=Release
